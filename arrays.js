e527bb17d5cdffc3918fbe08ba7c5a296f8c4c9b
shamima-wd <=>
2025-08-25 09:01:17 +0200

    opgave 1løst

--- arrays.js
+++ arrays.js
@@ -30,7 +30,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // --- Løs opgaverne herunder ---
   //opgave 1: Med udgangspunkt i numbersArray, skal du oprette et nyt array, som indeholder alle de tal som er større end 20.
-
+  const myArray = numbersArray.filter((num) => num > 20);
+  console.log(myArray);
   // Opgave 2
   // Tilføj katten "Sniffles" til catsArray.
   // Udskriv arrayet i konsollen.
