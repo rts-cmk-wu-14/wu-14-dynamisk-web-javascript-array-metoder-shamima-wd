387fa19de197f426c0bdb96ba4d05b40400c5900
shamima-wd <=>
2025-08-26 10:42:13 +0200

    opgave 7

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // --- Løs opgaverne herunder ---
   //opgave 1: Med udgangspunkt i numbersArray, skal du oprette et nyt array, som indeholder alle de tal som er større end 20.
   const myArray = numbersArray.filter((num) => num > 20);
-  console.log(myArray);
+  console.log("Opgave 1", myArray);
   // Opgave 2
   // Tilføj katten "Sniffles" til catsArray.
   // Udskriv arrayet i konsollen.
@@ -39,8 +39,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   //
   catsArray.push("Sniffles");
+  console.log("Opgave 2", catsArray);
 
-  console.log(catsArray);
   // Opgave 3a
   // Check om der i dogsArray er en hund som hedder "Trixie".
   // Udskriv "true" eller "false" i konsollen afhængig af svaret.
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const hasTrixie = dogsArray.includes("Trixie");
 
-  console.log(hasTrixie);
+  console.log("Opgave 3a", hasTrixie);
 
   // Opgave 3b
   // Check om der i dogsArray er en hund som hedder "Baxter".
@@ -57,23 +57,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const hasBaxter = dogsArray.some((dog) => dog === "Baxter");
 
-  console.log(hasBaxter);
+  console.log("Opgave 3b", hasBaxter);
 
   //     Opgave 4a
   // Check om der i catsArray er en kat som hedder "Bagheera".
   // Udskriv katten i konsollen hvis den findes og ellers "undefined".
-  // console.log("Opgave 4a", ...)
 
-  const bagheera = catsArray.find((cat) => cat.name === "Bagheera");
-  console.log(bagheera);
+  const bagheera = catsArray.find((cat) => cat === "Bagheera");
+  // console.log(bagheera);
+  console.log("Opgave 4a", bagheera);
 
   // Opgave 4b
   // Check om der i catsArray er en kat som hedder "Salem".
   // Udskriv katten i konsollen hvis den findes og ellers "undefined".
   // console.log("Opgave 4b", ...)
-  const foundCat = catsArray.find((cat) => cat.name === "Salem");
-
-  console.log(foundCat);
+  const foundCat = catsArray.find((cat) => cat === "Salem");
+  console.log("Opgave 4b", foundCat);
 
   //     Opgave 5
   // Med udgangspunkt i numbersArray, skal du oprette et nyt array, som indeholder alle tallene i arrayet ganget med 3.
@@ -81,7 +80,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // console.log("Opgave 5", ...)
 
   const multipliedArray = numbersArray.map((num) => num * 3);
-  console.log(multipliedArray);
+  console.log("Opgave 5", multipliedArray);
 
   //     Opgave 6
   // Udskriv alle hundene i dogsArray i én lang streng.
@@ -90,15 +89,18 @@ document.addEventListener("DOMContentLoaded", function () {
   // console.log("Opgave 6", ...)
 
   const allDogs = dogsArray.join("");
-
-  console.log(allDogs);
+  console.log("Opgave 6", allDogs);
   //     Opgave 7a
   // Find ud af hvilket index "Mango" har i fruitsArray.
   // Udskriv indexet i konsollen.
+  const mangoIndex = fruitsArray.indexOf("Mango");
+  console.log("Opgave 7a", mangoIndex);
 
   // Opgave 7b
   // Find ud af hvilket index "Blåbær" har i fruitsArray.
   // Udskriv indexet i konsollen.
+  const blueberryIndex = fruitsArray.indexOf("Blåbær");
+  console.log("Opgave 7b", blueberryIndex);
 
   // Opgave 8
   // Lad os forestille os, at dogsArray er en liste af tilmeldte hunde til en udstilling.
